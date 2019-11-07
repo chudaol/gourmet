464a63086f0ca85c720f70bbf0ea87cfd412a3b5
Olga Filipova <chudaol@gmail.com>
2019-11-08 00:56:07 +0100

    made orange orange

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Gourmets Delight</a>
+      <a class="navbar-brand text-primary" href="#">Smooth Orange</a>
       <a class="btn btn-primary" href="#email">Tell me more</a>
     </div>
   </nav>
