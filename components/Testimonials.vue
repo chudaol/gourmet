15e4bc2d9feb6cc42cd077a3235392b1b056674e
Olga Filipova <chudaol@gmail.com>
2019-11-08 01:01:07 +0100

    made orange orange

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -6,22 +6,22 @@
       <div class="row">
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-            <h5>Peter Eichhorn</h5>
+            <img class="border border-primary shadow-none img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
+            <h5 class="text-primary">Peter Eichhorn</h5>
             <p class="font-weight-light mb-0">Author of 16 books on food & beverage. Moderator and juror at Beverage and Bar Competitions.</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-            <h5>Alissa Rentowitsch</h5>
+            <img class="border border-primary shadow-none img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
+            <h5 class="text-primary">Alissa Rentowitsch</h5>
             <p class="font-weight-light mb-0">Publisher of gourmet magazines (print & online). Consultant in media marketing strategies with core competence niche markets.</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-            <h5>David Hoener</h5>
+            <img class="border border-primary shadow-none img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
+            <h5 class="text-primary">David Hoener</h5>
             <p class="font-weight-light mb-0">Book author, an NGO-founder. Knows everything about eating and drinking. Passionate cigar lover.</p>
           </div>
         </div>
